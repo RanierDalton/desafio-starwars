6b9ee2dbd0b4c28cf785303b4a8265131b606163
RanierDalton <ranier.couto@sptech.school>
2025-06-05 19:49:59 -0300

    fix: arumando os csv's do ep4 e ep5

--- wordCloud.R
+++ wordCloud.R
@@ -1,8 +1,8 @@
 install.packages("wordcloud2")
 library(wordcloud2)
 
-ep5_h <- read.csv("data/palavras-personagens/EP5/HAN.csv")
+ep6_t <- read.csv("data/palavras-personagens/EP6/THREEPIO.csv")
 
-ep5_h <- subset(ep5_h, !ep5_h$palavra %in% c("lando","vai","vamos","tudo","acho","onde", "pode", "jabba", "ohhh", "the", "tão", "vou","vão","artoo"))
+ep6_t <- subset(ep6_t, !ep6_t$palavra %in% c("lando","vai","vamos","tudo","acho","onde", "pode", "jabba", "ohhh", "the", "tão", "vou","vão","artoo", "posso","então"))
 
-wordcloud2(ep5_h)
+wordcloud2(ep6_t)
